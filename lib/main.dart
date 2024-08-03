8e07b9d8a381db01dfbe637ed2707ce54cc9475d
Sabareesh <sabareesh5757@gmail.com>
2024-08-03 12:34:49 +0530

    add: first app

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:demo/keys/keys.dart';
+import 'package:flutter/material.dart';
+
+//import 'package:demo/ui_updates_demo.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const Keys(),
+      ),
+    );
+  }
+}
